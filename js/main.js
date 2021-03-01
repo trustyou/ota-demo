b3b2b05bffdb6fe2fa083309936d98c6d0538e5a
Radu Luchian <radu.luchian@trustyou.com>
2021-03-01 15:13:21 +0100

    Created search results page, individual page styles.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-
-
 $(document).ready(function(){
 
+	// Ger search location from URL, submitted via search field
 	$searchLocation = (location.search.split(name + '=')[1] || '').split('&')[0];
 	$(document).find('#search-location').html($searchLocation);
 
