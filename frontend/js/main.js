e311d482ffd00471367751ebd7b7e9202d698ef8
Radu Luchian <radu.luchian@trustyou.com>
2021-03-02 13:31:43 +0100

    Implemented toggling of preference panel for search form

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,6 +2,23 @@ $(document).ready(function(){
 
 	// Ger search location from URL, submitted via search field
 	$searchLocation = (location.search.split(name + '=')[1] || '').split('&')[0];
-	$(document).find('#search-location').html($searchLocation);
+	$('#search-location').html($searchLocation);
+
+	$searchPreferences = $('#search-preferences');
+	$searchToggle = $('#search-toggle');
+
+	// Expand/collapse search prefences panel
+    $searchToggle.on('click', function(event) {
+		$searchPreferences.toggleClass('is-open');
+
+		if($searchPreferences.hasClass('is-open')) {
+			$searchToggle.find('span').text('Close search preferences');
+			$searchToggle.find('.ty-icon').attr('class', 'ty-icon ty-icon-chevron-up');
+		}
+		else {
+			$searchToggle.find('span').text('Adjust search preferences');
+			$searchToggle.find('.ty-icon').attr('class', 'ty-icon ty-icon-chevron-down');
+		}
+	})
 
 });
