532f11b59c77ca16f30aed02ac0701474e62c063
Cristian Constantinescu <cristian.constantinescu@trustyou.net>
2021-03-02 08:46:49 +0200

    Troubleshooting: add back postgres connection

--- backend/ota_demo_api/main.py
+++ backend/ota_demo_api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
 import ota_demo_api.consts as consts
+from ota_demo_api.persistence.database import database
 from ota_demo_api.routers import search
 
 app = FastAPI()
@@ -20,6 +21,16 @@ async def health_check():
     return {
         consts.WEB_SERVER: consts.UP
     }
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 
 app.include_router(
