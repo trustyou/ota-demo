aad3f80f2423311c01853ac83cd7b3e6f502c888
Duy Nguyen Hoang <duy.nguyen@trustyou.net>
2021-03-17 16:48:26 +0700

    Add pagination

--- includes/result-map.js
+++ includes/result-map.js
@@ -2,27 +2,15 @@ var map = L.map('search-map').setView([48.1019351, 11.53698539037037], 11);
 var markers = [];
 
 function getIcon(scoreDescription) {
-    const idx = scoreDescription.toLowerCase().replaceAll(" ", "");
+    const iconClass = scoreDescription.toLowerCase().replaceAll(" ", "");
 
-    const icons = {
-        excellent: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
-        verygood: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-gold.png',
-        fair: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-yellow.png',
-        good: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png',
-        poor: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-grey.png',
-    }
-    var icon = icons[idx];
-    if (!icon) {
-        icon = icons[fair];
-    }
-
-    return new L.Icon({
-        iconUrl: icon,
+    return L.divIcon({
+        className: "trustyou-marker",
         shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
+        iconAnchor: [0, 24],
+        labelAnchor: [-6, 0],
+        popupAnchor: [-6, -30],
+        html: `<span class="map-marker marker-${iconClass}" />`
     });
 }
 
@@ -43,12 +31,6 @@ function addMarker(tyId, scoreDescription, lat, lon, popupText) {
     }
     cleanMarker(tyId);
     markers[tyId] = L.marker([lon, lat], {icon: getIcon(scoreDescription)}).bindPopup(popupText).addTo(map);
-}
-
-function cleanMarkers() {
-    if (map && markers) {
-        Object.entries(markers).map(([ty_id, _]) => cleanMarker(ty_id));
-    }
 }
 
 function cleanMarker(ty_id) {
