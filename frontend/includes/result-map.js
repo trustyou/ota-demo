5c2bbd290c12fd07f65cda4e77b074fb645ea5c1
Duy Nguyen Hoang <duy.nguyen@trustyou.net>
2021-03-17 19:56:29 +0700

    Use light all layer for map

--- frontend/includes/result-map.js
+++ frontend/includes/result-map.js
@@ -18,7 +18,7 @@ function buildMap(lat, lon) {
     }
     map = L.map('search-map').setView([lat, lon], 13);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 }
